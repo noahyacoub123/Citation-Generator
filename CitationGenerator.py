30a605af0c94709e68ef8a72b94ccb0493b20673
Noah Yacoub <noahyacoub123@gmail.com>
2023-06-04 15:32:38 -0400

    test

--- CitationGenerator.py
+++ CitationGenerator.py
@@ -3,39 +3,41 @@
 import tkinter as tk
 
 mw = tk.Tk()
-mw.geometry('400x400')
+mw.geometry('300x200')
 mw.title("BEST Citation Generator")
-greeting = tk.Label(text="Hello! Please pick which format you would like to use!")
+greeting = tk.Label(text="Hello! Please pick which format you would like to use!") #Greeting message prompting user to select which format they would like to use
 greeting.grid(row = 0)
 
 
-def OpenNewWindow_op1(): #Opens a new window for op1
+def OpenNewWindow_op1(): #Opens a new window for MLA9
     nw1 = tk.Toplevel(mw)
-    nw1.title("op1 Citation Generator")
+    nw1.title("MLA9 Citation Generator")
     nw1.geometry('400x400')
+    tk.Label(nw1, text="Please input all the information known below!").pack()
+    tk.Label(nw1, text="First name")
+    firstname_1 = tk.entry()
 
-
-def OpenNewWindow_op2(): #Opens a new window for op2
+def OpenNewWindow_op2(): #Opens a new window for APA7
     nw2 = tk.Toplevel(mw)
-    nw2.title("op2 Citation Generator")
+    nw2.title("APA7 Citation Generator")
     nw2.geometry('400x400')
+    tk.Label(nw2, text="Please input all the information known below!").pack()
+    tk.Label(nw2)
 
-
-def OpenNewWindow_op3(): #Opens a new window for op3
+def OpenNewWindow_op3(): #Opens a new window for Chicago style
     nw3 = tk.Toplevel(mw)
-    nw3.title("op3 Citation Generator")
+    nw3.title("CHICAGO Citation Generator")
     nw3.geometry('400x400')
+    tk.Label(nw3, text="Please input all the information known below!").pack()
 
 
-op1 = tk.Button(mw, text='Op1', command=OpenNewWindow_op1)
-op1.grid(row = 1, column = 0, padx = 10)
+op1 = tk.Button(mw, text='MLA9', command=OpenNewWindow_op1)
+op1.grid(row = 1, column = 0, padx = 5, pady=5)
 
-op2 = tk.Button(mw, text='Op2', command=OpenNewWindow_op2)
-op2.grid(row = 1, column = 1, padx = 10)
+op2 = tk.Button(mw, text='APA7', command=OpenNewWindow_op2)
+op2.grid(row = 2, column = 0, padx = 5, pady=5)
 
-op3 = tk.Button(mw, text='Op3', command=OpenNewWindow_op3)
-op3.grid(row = 1, column = 2, padx = 10)
-
-
+op3 = tk.Button(mw, text='CHICAGO', command=OpenNewWindow_op3)
+op3.grid(row = 3, column = 0, padx = 5, pady=5)
 
 mw.mainloop()
